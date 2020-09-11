2b761f451ea64dfa7ab853638dc5f115f2dee331
Maksym Korbiak <Maksym_Korbiak@epam.com>
2020-09-11 17:01:18 +0300

    init back

---
+++ api/src/main/resources/static/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE item;
+
+CREATE TABLE item
+(
+    id        SERIAL PRIMARY KEY,
+    name     VARCHAR(32) NOT NULL
+);
+
+
+INSERT INTO item(name)
+VALUES ('SWIMMING'),
+       ('YOGA'),
+       ('RUNNING');
