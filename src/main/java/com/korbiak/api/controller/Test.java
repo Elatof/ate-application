d1bd22768a8b53f4fd922947257bf08a2ac8bb74
Maksym Korbiak <Maksym_Korbiak@epam.com>
2020-09-15 16:07:19 +0300

    test 5

---
+++ src/main/java/com/korbiak/api/controller/Test.java
@@ -0,0 +1,13 @@
+package com.korbiak.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @GetMapping("test")
+    public String getTest(){
+        return "ok";
+    }
+}
